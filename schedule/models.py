db17277f82d01a1064b2747481190b93b0a0a6ab
Rocky Ken <rocky_8916@hotmail.com>
2022-11-18 16:27:09 -0800

    Made some schdule fields nullable

--- schedule/models.py
+++ schedule/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 class Schedule(models.Model):
     Subject = models.CharField(max_length=100)
-    Location = models.CharField(max_length=100)
+    Location = models.CharField(max_length=100, blank=True, null=True)
     StartTime = models.DateTimeField()
     EndTime = models.DateTimeField()
     IsAllDay = models.BooleanField(default=False)
-    Status = models.CharField(max_length=100)
-    Priority = models.CharField(max_length=100)
-    CategoryColor = models.CharField(max_length=100)
+    Status = models.CharField(max_length=100, blank=True, null=True)
+    Priority = models.CharField(max_length=100, blank=True, null=True)
+    CategoryColor = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.Subject
